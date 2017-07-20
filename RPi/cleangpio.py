704fa670e37015373ad023319a94ca2fd3270c53
Thiagofelis <thiagofelis-br@hotmail.com>
2017-07-20 14:53:05 -0300

    correcao requerimentos de 'ping', temperatura e aviso pra quando a lata estiver na temp desejada ja funcionam

---
+++ RPi/cleangpio.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup (11, GPIO.OUT)
+GPIO.setup (13, GPIO.OUT)
+GPIO.setup (15, GPIO.IN)
+GPIO.output (11, 0)
+GPIO.output (13, 1)
+
+
+GPIO.cleanup()
+
